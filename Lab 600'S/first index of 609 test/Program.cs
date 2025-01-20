b8b7e1c5e1ae8cec40c81ba170ed3f0e349fe893
Travis-O-Weber <33355911+Travis-O-Weber@users.noreply.github.com>
2025-01-20 17:10:47 -0600

    Add files via upload

---
+++ Lab 600'S/first index of 609 test/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: legol
+ * Date: 5/18/2016
+ * Time: 3:04 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace first_index_of_609_test
+{
+	class Program
+	{
+		public static void Main(string[] args)
+			{
+			Console.WriteLine("Please type the word you want displayed backwards");
+			string og = Console.ReadLine();
+			og = og.ToLower();
+ 
+            string first = og.IndexOf(0);
+
+            foreach (var word in og.Split(' '))
+            {
+                string temp = " ";
+                foreach (var ch in word.ToCharArray())
+                {
+                    temp = ch + temp;
+                }
+                strrev = temp + strrev + " "; 
+            }
+            
+            Console.WriteLine(strrev);
+            Console.ReadLine();
+
+			// TODO: Implement Functionality Here
+			
+			Console.ReadKey(true);
+		}
+	}
+}
