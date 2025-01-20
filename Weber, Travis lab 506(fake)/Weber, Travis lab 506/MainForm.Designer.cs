b87c51e87df78a8898cf8b99f6498e2e2a6c94f2
Travis-O-Weber <33355911+Travis-O-Weber@users.noreply.github.com>
2025-01-20 16:38:30 -0600

    Add files via upload

---
+++ Weber, Travis lab 506(fake)/Weber, Travis lab 506/MainForm.Designer.cs
@@ -0,0 +1,296 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: s570086
+ * Date: 3/8/2016
+ * Time: 2:36 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Weber__Travis_lab_506
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.PictureBox pictureBox5;
+		private System.Windows.Forms.PictureBox pictureBox6;
+		private System.Windows.Forms.PictureBox pictureBox7;
+		private System.Windows.Forms.PictureBox pictureBox8;
+		private System.Windows.Forms.PictureBox pictureBox9;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.pictureBox5 = new System.Windows.Forms.PictureBox();
+			this.pictureBox6 = new System.Windows.Forms.PictureBox();
+			this.pictureBox7 = new System.Windows.Forms.PictureBox();
+			this.pictureBox8 = new System.Windows.Forms.PictureBox();
+			this.pictureBox9 = new System.Windows.Forms.PictureBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(930, 412);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(698, 112);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 55F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label1.Location = new System.Drawing.Point(0, 69);
+			this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(1530, 165);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Ubbi Dubbi Converter";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label2.Location = new System.Drawing.Point(164, 412);
+			this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(702, 115);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Type the Word You Want to Convert";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.label2.Click += new System.EventHandler(this.Label2Click);
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label3.Location = new System.Drawing.Point(164, 962);
+			this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(702, 115);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Your New Word is";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label4.Location = new System.Drawing.Point(930, 829);
+			this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(702, 115);
+			this.label4.TabIndex = 3;
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.label4.Click += new System.EventHandler(this.Label4Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(164, 669);
+			this.button1.Margin = new System.Windows.Forms.Padding(6);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(702, 154);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+			this.pictureBox4.Location = new System.Drawing.Point(0, 313);
+			this.pictureBox4.Margin = new System.Windows.Forms.Padding(6);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(606, 327);
+			this.pictureBox4.TabIndex = 9;
+			this.pictureBox4.TabStop = false;
+			this.pictureBox4.Visible = false;
+			this.pictureBox4.Click += new System.EventHandler(this.PictureBox4Click);
+			// 
+			// pictureBox5
+			// 
+			this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+			this.pictureBox5.Location = new System.Drawing.Point(586, 313);
+			this.pictureBox5.Margin = new System.Windows.Forms.Padding(6);
+			this.pictureBox5.Name = "pictureBox5";
+			this.pictureBox5.Size = new System.Drawing.Size(606, 327);
+			this.pictureBox5.TabIndex = 10;
+			this.pictureBox5.TabStop = false;
+			this.pictureBox5.Visible = false;
+			this.pictureBox5.Click += new System.EventHandler(this.PictureBox5Click);
+			// 
+			// pictureBox6
+			// 
+			this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+			this.pictureBox6.Location = new System.Drawing.Point(1172, 313);
+			this.pictureBox6.Margin = new System.Windows.Forms.Padding(6);
+			this.pictureBox6.Name = "pictureBox6";
+			this.pictureBox6.Size = new System.Drawing.Size(606, 327);
+			this.pictureBox6.TabIndex = 11;
+			this.pictureBox6.TabStop = false;
+			this.pictureBox6.Visible = false;
+			this.pictureBox6.Click += new System.EventHandler(this.PictureBox6Click);
+			// 
+			// pictureBox7
+			// 
+			this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+			this.pictureBox7.Location = new System.Drawing.Point(0, 629);
+			this.pictureBox7.Margin = new System.Windows.Forms.Padding(6);
+			this.pictureBox7.Name = "pictureBox7";
+			this.pictureBox7.Size = new System.Drawing.Size(606, 327);
+			this.pictureBox7.TabIndex = 12;
+			this.pictureBox7.TabStop = false;
+			this.pictureBox7.Visible = false;
+			this.pictureBox7.Click += new System.EventHandler(this.PictureBox7Click);
+			// 
+			// pictureBox8
+			// 
+			this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+			this.pictureBox8.Location = new System.Drawing.Point(1170, 732);
+			this.pictureBox8.Margin = new System.Windows.Forms.Padding(6);
+			this.pictureBox8.Name = "pictureBox8";
+			this.pictureBox8.Size = new System.Drawing.Size(606, 327);
+			this.pictureBox8.TabIndex = 13;
+			this.pictureBox8.TabStop = false;
+			this.pictureBox8.Visible = false;
+			this.pictureBox8.Click += new System.EventHandler(this.PictureBox8Click);
+			// 
+			// pictureBox9
+			// 
+			this.pictureBox9.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox9.Image")));
+			this.pictureBox9.Location = new System.Drawing.Point(1172, 629);
+			this.pictureBox9.Margin = new System.Windows.Forms.Padding(6);
+			this.pictureBox9.Name = "pictureBox9";
+			this.pictureBox9.Size = new System.Drawing.Size(606, 327);
+			this.pictureBox9.TabIndex = 14;
+			this.pictureBox9.TabStop = false;
+			this.pictureBox9.Click += new System.EventHandler(this.PictureBox9Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(606, 327);
+			this.pictureBox1.TabIndex = 15;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Visible = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(586, 0);
+			this.pictureBox2.Margin = new System.Windows.Forms.Padding(6);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(606, 327);
+			this.pictureBox2.TabIndex = 16;
+			this.pictureBox2.TabStop = false;
+			this.pictureBox2.Visible = false;
+			this.pictureBox2.Click += new System.EventHandler(this.PictureBox2Click);
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(1172, 0);
+			this.pictureBox3.Margin = new System.Windows.Forms.Padding(6);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(606, 327);
+			this.pictureBox3.TabIndex = 17;
+			this.pictureBox3.TabStop = false;
+			this.pictureBox3.Visible = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Maroon;
+			this.ClientSize = new System.Drawing.Size(1791, 1065);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.pictureBox9);
+			this.Controls.Add(this.pictureBox8);
+			this.Controls.Add(this.pictureBox7);
+			this.Controls.Add(this.pictureBox6);
+			this.Controls.Add(this.pictureBox5);
+			this.Controls.Add(this.pictureBox4);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Margin = new System.Windows.Forms.Padding(6);
+			this.Name = "MainForm";
+			this.Text = "Weber, Travis lab 506";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ Weber, Travis lab 506(fake)/Weber, Travis lab 506/MainForm.cs
@@ -0,0 +1,207 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: s570086
+ * Date: 3/8/2016
+ * Time: 2:36 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media; 
+
+namespace Weber__Travis_lab_506
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+	
+	
+		System.Media.SoundPlayer startSoundPlayer = new System.Media.SoundPlayer(@"C:\Users\legol\Documents\School stuff\Sound Effects - And His Name Is John Cena.wav");//laptop
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Label1Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Label2Click(object sender, EventArgs e)
+		{
+	
+		}
+		void PictureBox1Click(object sender, EventArgs e)
+		{
+	
+		}
+		void PictureBox2Click(object sender, EventArgs e)
+		{
+	
+		}
+		void PictureBox3Click(object sender, EventArgs e)
+		{
+		}
+		void PictureBox4Click(object sender, EventArgs e)
+		{
+	
+		}
+		void PictureBox5Click(object sender, EventArgs e)
+		{
+	
+		}
+		void PictureBox6Click(object sender, EventArgs e)
+		{
+	
+		}
+		void PictureBox7Click(object sender, EventArgs e)
+		{
+	
+		}
+		void PictureBox8Click(object sender, EventArgs e)
+		{
+	
+		}
+		void PictureBox9Click(object sender, EventArgs e)
+		{
+	
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			
+			
+		}
+		void Label4Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			
+		
+			string wordOG = (textBox1.Text);
+			string words = wordOG.ToLower();
+			int length = wordOG.Length ;
+			
+			string ub = "ub";
+			string vowel1 = "a";
+			string vowel2 = "e";
+			string vowel3 = "i";
+			string vowel4 = "o";
+			string vowel5 = "u";
+			string vowel6 = "y";
+			string trigger = "hello";
+			
+			 if 
+				(words == trigger)
+				{
+				pictureBox1.Visible = true;
+				pictureBox2.Visible = true;
+				pictureBox3.Visible = true;
+				pictureBox4.Visible = true;
+				pictureBox5.Visible = true;
+				pictureBox6.Visible = true;
+				pictureBox7.Visible = true;
+				pictureBox8.Visible = true;
+				pictureBox9.Visible = true;
+				
+				startSoundPlayer.Play();
+				}
+			else if
+				(wordOG.Substring(0,1) == vowel1 ||
+			    wordOG.Substring(0,1) == vowel2 ||
+			    wordOG.Substring(0,1) == vowel3 ||
+			    wordOG.Substring(0,1) == vowel4 ||
+			    wordOG.Substring(0,1) == vowel5 |
+			    wordOG.Substring(0,1) == vowel6 )
+			   
+				{
+				words = words.Insert(0,ub);
+				label4.Text = words;
+				}
+			else if 
+				(wordOG.Substring(1,1) == vowel1 ||
+			    wordOG.Substring(1,1) == vowel2 ||
+			    wordOG.Substring(1,1) == vowel3 ||
+			    wordOG.Substring(1,1) == vowel4 ||
+			    wordOG.Substring(1,1) == vowel5 ||
+			    wordOG.Substring(1,1) == vowel6 )
+	
+				{
+				words = words.Insert(1,ub);
+				label4.Text = words;
+				}
+			else if 
+				(wordOG.Substring(2,1) == vowel1 ||
+			    wordOG.Substring(2,1) == vowel2 ||
+			    wordOG.Substring(2,1) == vowel3 ||
+			    wordOG.Substring(2,1) == vowel4 ||
+			    wordOG.Substring(2,1) == vowel5 ||
+			    wordOG.Substring(2,1) == vowel6 )
+			
+				{
+				words = words.Insert(2,ub);
+				label4.Text = words;
+				}
+			else if 
+				(wordOG.Substring(3,1) == vowel1 ||
+			    wordOG.Substring(3,1) == vowel2 ||
+			    wordOG.Substring(3,1) == vowel3 ||
+			    wordOG.Substring(3,1) == vowel4 ||
+			    wordOG.Substring(3,1) == vowel5 ||
+			    wordOG.Substring(3,1) == vowel6 )
+			
+				{
+				words = words.Insert(3,ub);
+				label4.Text = words;
+				}
+			else if 
+				(wordOG.Substring(4,1) == vowel1 ||
+			    wordOG.Substring(4,1) == vowel2 ||
+			    wordOG.Substring(4,1) == vowel3 ||
+			    wordOG.Substring(4,1) == vowel4 ||
+			    wordOG.Substring(4,1) == vowel5 ||
+			    wordOG.Substring(4,1) == vowel6 )
+			
+				{
+				words = words.Insert(4,ub);
+				label4.Text = words;
+				}
+			else if 
+				(wordOG.Substring(5,1) == vowel1 ||
+			    wordOG.Substring(5,1) == vowel2 ||
+			    wordOG.Substring(5,1) == vowel3 ||
+			    wordOG.Substring(5,1) == vowel4 ||
+			    wordOG.Substring(5,1) == vowel5 ||
+			    wordOG.Substring(5,1) == vowel6 )
+		
+				{
+				words = words.Insert(5,ub);
+				label4.Text = words;
+				}
+			else if 
+				(wordOG.Substring(6,1) == vowel1 ||
+			    wordOG.Substring(6,1) == vowel2 ||
+			    wordOG.Substring(6,1) == vowel3 ||
+			    wordOG.Substring(6,1) == vowel4 ||
+			    wordOG.Substring(6,1) == vowel5 ||
+			    wordOG.Substring(6,1) == vowel6 )
+		
+				{
+				words = words.Insert(6,ub);
+				label4.Text = words;
+				}
+			
+		}	
+	}
+}
