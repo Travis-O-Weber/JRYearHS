6c934d304347599d2f8642c33132663bfee1d118
Travis-O-Weber <33355911+Travis-O-Weber@users.noreply.github.com>
2025-01-20 16:57:23 -0600

    Add files via upload

---
+++ worksheets/Test 100 down to 5 by 5/Test 100 down to 5 by 5/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: legol
+ * Date: 4/23/2016
+ * Time: 5:38 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Test_100_down_to_5_by_5
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			int p = 100;						/*14.*/
+			
+			while (p > 0)
+			{
+				Console.WriteLine(p);
+				p -= 5;
+			}
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ worksheets/Test 3 to 99 by 3s/Test 3 to 99 by 3s/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: legol
+ * Date: 4/23/2016
+ * Time: 5:44 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Test_3_to_99_by_3s
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int num = 0;
+			
+			for (num <= 99);
+			
+				Console.WriteLine(num);
+				num += 3;}
+			}
+			Console.ReadKey(true);
+		}
+	}
+}
