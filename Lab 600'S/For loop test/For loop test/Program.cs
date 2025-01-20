be6e57d5aaac68731da30280cee9ec0be0a6d26f
Travis-O-Weber <33355911+Travis-O-Weber@users.noreply.github.com>
2025-01-20 17:04:56 -0600

    Add files via upload

---
+++ Lab 600'S/For loop test/For loop test/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: legol
+ * Date: 5/4/2016
+ * Time: 2:53 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace For_loop_test
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+ 			for (int counter = 0; counter <= 1000; counter ++)
+ 			{
+ 				Console.WriteLine(counter);
+ 			}
+			Console.ReadKey(true);
+		}
+	}
+}
