61ca4615b8255fd75ae0a905038934d6b56663a6
Travis-O-Weber <33355911+Travis-O-Weber@users.noreply.github.com>
2025-01-20 16:17:20 -0600

    Add files via upload

---
+++ Sound test/Sound test/MainForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Travis
+ * Date: 3/2/2016
+ * Time: 10:20 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Sound_test
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.CheckBox duelStatebutton;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.duelStatebutton = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// duelStatebutton
+			// 
+			this.duelStatebutton.Location = new System.Drawing.Point(55, 45);
+			this.duelStatebutton.Name = "duelStatebutton";
+			this.duelStatebutton.Size = new System.Drawing.Size(104, 24);
+			this.duelStatebutton.TabIndex = 0;
+			this.duelStatebutton.Text = "Play";
+			this.duelStatebutton.UseVisualStyleBackColor = true;
+			this.duelStatebutton.CheckedChanged += new System.EventHandler(this.DuelStatebuttonCheckedChanged);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(524, 316);
+			this.Controls.Add(this.duelStatebutton);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "MainForm";
+			this.Text = "Sound test";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ Sound test/Sound test/MainForm.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Travis
+ * Date: 3/2/2016
+ * Time: 10:20 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Sound_test
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		
+		private SoundPlayer _soundplayer;
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			private SoundPlayer _soundplayer;
+		
+			_soundplayer = new SoundPlayer("Call On Me.wav"); 
+			
+			System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+player.Play();
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+	
+		}
+		void DuelStatebuttonCheckedChanged(object sender, EventArgs e)
+		{
+			if (duelStatebutton.Checked)  
+			{
+				duelStatebutton.Text = "Stop";
+				_soundplayer.Play();
+			}
+			
+				else 
+			{
+					duelStatebutton.Text = "Play";
+				_soundplayer.Stop();
+					
+			}
+		}
+	}
+}
+
